6aa30e2628ccaf10f24228cb6ed0e82e5d3630b0
Ganesh Palaniappan <pl.ganesh@gmail.com>
2017-02-09 19:06:48 +0530

    Added animation feature

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,17 @@
 console.log('Loaded!');
+
+var marginLeft = 0;
+var madi_img = document.getElementById("madi");
+
+function moveRight() {
+    marginLeft += 10;
+    madi_img.style.marginLeft = marginLeft + "px";
+};
+    
+function moveImage() {
+    madi_img.onclick = function () {
+        var interval = setInterval(moveRight, 50);
+    };
+};
+
+moveImage();
