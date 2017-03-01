b7c7ecad1994d0ea1e298d18334608080898170a
Ganesh Palaniappan <ganeshpalaniappan@users.noreply.github.com>
2017-03-01 22:20:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -31,15 +31,19 @@ app.get('/', function (req, res) {
 var pool = new Pool(config);
 app.get('/articles/:articleName', function (req, res) {
     var articleName = req.params.articleName;
+    console.log("Article Name is: " + articleName);
     pool.query("SELECT * FROM article WHERE title = $1", [articleName], function (err, result) {
         if (err) {
+            console.log("In Error Block - connection issue? " + err.toString);
             res.send(err.toString);
         }
         else {
             if (result.rows.length === 0) {
+                console.log("Empty 0th row");
                 res.send("Article not found");
             }
             else {
+                console.log("Creating template");
                 res.send(createPageFromTemplate(result.rows[0],"Article-Template.html"));
             }
         }
